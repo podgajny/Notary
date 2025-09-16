cf7d5e1659bfdb07311c2f14169b021e0c00b167
Mg <bartosz.magdziarz@gmail.com>
2025-09-16 19:04:05 +0200

    update 2

--- src/components/NoteDisplay.vue
+++ src/components/NoteDisplay.vue
@@ -14,7 +14,7 @@
 import { ref } from 'vue'
 
 // Statyczne dane dla MVP
-const noteTitle = ref('TEST: Zmieniony tytuł notatki - wersja testowa')
+const noteTitle = ref('TEST: Zmieniony tytuł notatki - wersja testowa2')
 const noteBody = ref('Dobry wieczór. Coś się... coś się popsuło i nie było mnie słychać, to powtórzę jeszcze raz. Wynik wyborczy KWW Stonogi to jest jakaś porażka, ja myślę że to polskie, kurwa głupie społeczeństwo, te kurwa banda imbecyli która głosowała na tych kurwów karakanów z PiSu to jest jakieś kurwa nieporozumienie. Jeśli tyle dla was znaczy... jeśli tyle dla was znaczy, ludzie takie zaangażowanie społeczne, jak moje gdzie postawiłem moją rodzinę, moje życie prywatne, biznes, wszystko inne i dla was tylko znaczyło to pierdolone 70 czy 80 tysięcy głosów, to was powinno się jebać kurwa, tak się powinno was jebać, jak tylko was może jebać PiS.')
 </script>
 
