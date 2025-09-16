b36e6b10473dd877fb462615e629de6e94da14f8
Wookieunderwood <l.podgajny@gmail.com>
2025-09-16 12:10:30 +0200

    Uzupełnienie treści notatki przykładem z zycia

--- Notary-web/src/components/NoteDisplay.vue
+++ Notary-web/src/components/NoteDisplay.vue
@@ -14,8 +14,8 @@
 import { ref } from 'vue'
 
 // Statyczne dane dla MVP
-const noteTitle = ref('Hello World')
-const noteBody = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+const noteTitle = ref('Coś się popsuło, nie było mnie słychać')
+const noteBody = ref('Dobry wieczór. Coś się... coś się popsuło i nie było mnie słychać, to powtórzę jeszcze raz. Wynik wyborczy KWW Stonogi to jest jakaś porażka, ja myślę że to polskie, kurwa głupie społeczeństwo, te kurwa banda imbecyli która głosowała na tych kurwów karakanów z PiSu to jest jakieś kurwa nieporozumienie. Jeśli tyle dla was znaczy... jeśli tyle dla was znaczy, ludzie takie zaangażowanie społeczne, jak moje gdzie postawiłem moją rodzinę, moje życie prywatne, biznes, wszystko inne i dla was tylko znaczyło to pierdolone 70 czy 80 tysięcy głosów, to was powinno się jebać kurwa, tak się powinno was jebać, jak tylko was może jebać PiS.')
 </script>
 
 <style scoped>
