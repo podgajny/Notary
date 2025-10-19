f6b6d15d0270f43d2ac9e60448d135012463db01
Mg <bartosz.magdziarz@gmail.com>
2025-10-19 10:13:40 +0200

    feat(routing): integrate editor and list on home view

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './assets/main.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "./assets/main.css";
+import App from "./App.vue";
+import { router } from "./router";
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+app.mount("#app");
